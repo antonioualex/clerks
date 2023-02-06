6c4b1c6dd0a6486c0e09225508f6e68419df239d
antonioualex <antonal01@yahoo.gr>
2023-02-06 20:50:45 +0200

    update Populate service to store the 5000 users in chunks of 100 using go routines, in order to fasten up the storage procedure

--- app/services/user.go
+++ app/services/user.go
@@ -1,6 +1,12 @@
 package services
 
-import "clerks/domain"
+import (
+	"clerks/domain"
+	"errors"
+	"fmt"
+	"log"
+	"sync"
+)
 
 type UserService struct {
 	ur domain.UserRepository
@@ -12,7 +18,42 @@ func (s UserService) Populate() error {
 	if err != nil {
 		return err
 	}
-	return s.ur.AddUsers(randomUsers)
+
+	chunkSize := 100
+	numChunks := len(randomUsers) / chunkSize
+	if len(randomUsers)%chunkSize != 0 {
+		numChunks++
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(numChunks)
+	errCh := make(chan error, numChunks)
+
+	for i := 0; i < numChunks; i++ {
+		go func(i int) {
+			start := i * chunkSize
+			end := (i + 1) * chunkSize
+			if end > len(randomUsers) {
+				end = len(randomUsers)
+			}
+			err := s.ur.AddUsers(randomUsers[start:end])
+			if err != nil {
+				log.Printf("Error storing chunk %d: %v", i, err)
+				errCh <- errors.New(fmt.Sprintf("Error storing chunk %d: %v", i, err))
+			}
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+	close(errCh)
+
+	numOfFailedChanks := len(errCh)
+	if numOfFailedChanks > 0 {
+		return errors.New(fmt.Sprintf("failed to store %d chunk of users out of %d", numOfFailedChanks, numChunks))
+	}
+
+	return nil
 }
 
 func (s UserService) GetUsers(email string, limit, offset int) ([]domain.User, int, int, error) {
